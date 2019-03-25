8694057e28e1d8990d64561b1b4eb630b0b0431e
bluefad <bluefad@126.com>
2019-03-25 10:19:00 +0800

    OpenStora

---
+++ wiki/indexdbf4.php
@@ -0,0 +1,16 @@
+; You're seeing this file because you're using Mediawiki's External Editor
+; feature. This is probably because you selected use external editor
+; in your preferences. To edit normally, either disable that preference
+; or go to the URL http://www.openstora.com/wiki/images/2/29/Stora_v3-1.png .
+; See http://www.mediawiki.org/wiki/Manual:External_editors for details.
+[Process]
+Type=Edit file
+Engine=MediaWiki
+Script=http://www.openstora.com/wiki/index.php
+Server=http://www.openstora.com
+Path=/wiki
+Special namespace=Special
+
+[File]
+Extension=png
+URL=http://www.openstora.com/wiki/images/2/29/Stora_v3-1.png
